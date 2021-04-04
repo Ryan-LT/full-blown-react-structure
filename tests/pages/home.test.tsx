42c004177b3f3834372b84ccc5017b283585987f
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-04-04 12:04:55 +0700

    test(home): test event handler

--- tests/pages/home.test.tsx
+++ tests/pages/home.test.tsx
@@ -1,20 +1,129 @@
 import React from 'react';
+import axios from 'axios';
 import '@testing-library/jest-dom';
+import MockAdapter from 'axios-mock-adapter';
 import { Home } from '@pages';
-import { cleanup, render } from '../renderTest';
+import { fireEvent } from '@testing-library/dom';
+import { cleanup, render, screen } from '../renderTest';
 
 afterEach(cleanup);
 
-describe('<Home />', () => {
-  it('should take a snapshot', () => {
-    const { asFragment } = render(<Home />);
+const mock = new MockAdapter(axios);
 
-    expect(asFragment()).toMatchSnapshot();
+describe('<Home />', () => {
+  describe('Simple Render', () => {
+    it('should take a snapshot', () => {
+      const { asFragment } = render(<Home />);
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should render without errors', () => {
+      const component = render(<Home />);
+
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should render without errors', () => {
-    const component = render(<Home />);
+  describe('Event Handler', () => {
+    beforeEach(() => {
+      mock.reset();
+    });
 
-    expect(component).toBeTruthy();
+    it('should be able to search and render options', async () => {
+      const mockResponse = [
+        {
+          title: 'London',
+          location_type: 'City',
+          woeid: 44418,
+          latt_long: '51.506321,-0.12714',
+        },
+        {
+          title: 'Barcelona',
+          location_type: 'City',
+          woeid: 753692,
+          latt_long: '41.385578,2.168740',
+        },
+        {
+          title: 'Long Beach',
+          location_type: 'City',
+          woeid: 2441472,
+          latt_long: '33.766720,-118.192398',
+        },
+      ];
+
+      mock
+        .onGet('/location/search/', { params: { query: 'lon' } })
+        .replyOnce(200, mockResponse);
+
+      const { container, getByText } = render(<Home />);
+
+      const input = container.querySelector('input');
+
+      expect(input).toBeTruthy();
+
+      fireEvent.change(input!, { target: { value: 'lon' } });
+
+      await screen.findByText('London');
+
+      expect(getByText('London')).toBeTruthy();
+      expect(getByText('Barcelona')).toBeTruthy();
+      expect(getByText('Long Beach')).toBeTruthy();
+    });
+
+    it('should be able to select an option and return result', async () => {
+      const mockSearchResponse = [
+        {
+          title: 'London',
+          location_type: 'City',
+          woeid: 44418,
+          latt_long: '51.506321,-0.12714',
+        },
+      ];
+
+      const mockGetResponse = {
+        consolidated_weather: [
+          {
+            id: 5102173131112448,
+            applicable_date: '2021-04-04',
+            min_temp: 2,
+            max_temp: 15,
+          },
+        ],
+        parent: {
+          title: 'England',
+          location_type: 'Region / State / Province',
+          woeid: 24554868,
+          latt_long: '52.883560,-1.974060',
+        },
+        title: 'London',
+      };
+
+      mock
+        .onGet('/location/search/', { params: { query: 'london' } })
+        .replyOnce(200, mockSearchResponse);
+      mock.onGet('/location/44418/').replyOnce(200, mockGetResponse);
+
+      const { container, getByText, getByTestId } = render(<Home />);
+
+      const input = container.querySelector('input');
+
+      expect(input).toBeTruthy();
+
+      fireEvent.change(input!, { target: { value: 'london' } });
+
+      await screen.findByText('London');
+
+      const option = getByText('London');
+
+      fireEvent.click(option);
+
+      await screen.findByText('London, England');
+
+      expect(getByText('London')).toBeTruthy();
+      expect(getByTestId('card-day')).toHaveTextContent('Sunday');
+      expect(getByTestId('max-temp')).toHaveTextContent('15');
+      expect(getByTestId('min-temp')).toHaveTextContent('2');
+    });
   });
 });
--- tests/store/slices/home.test.ts
+++ tests/store/slices/home.test.ts
@@ -10,103 +10,105 @@ const mock = new MockAdapter(axios);
 
 const store = createApp();
 
-describe('Home Reducer', () => {
-  test('should return initial state on first run', () => {
-    const nextState = initialState;
-    const action = {} as PayloadAction;
-    const result = homeReducer(undefined, action);
-
-    expect(result).toEqual(nextState);
-  });
-});
-
 describe('Home Slice', () => {
-  beforeEach(() => {
-    mock.reset();
+  describe('Home Reducer', () => {
+    test('should return initial state on first run', () => {
+      const nextState = initialState;
+      const action = {} as PayloadAction;
+      const result = homeReducer(undefined, action);
+
+      expect(result).toEqual(nextState);
+    });
   });
 
-  test('should dispatch search location and return success state', async () => {
-    const expectedResponse = [
-      {
-        title: 'London',
-        location_type: 'City',
-        woeid: 44418,
-        latt_long: '51.506321,-0.12714',
-      },
-    ];
+  describe('Home Action', () => {
+    beforeEach(() => {
+      mock.reset();
+    });
 
-    mock
-      .onGet('/location/search/', { params: { query: 'london' } })
-      .replyOnce(200, expectedResponse);
-
-    expect(store.getState().home.isSearching).toBeFalsy();
-
-    const promise = store.dispatch(searchLocation({ query: 'london' }));
-
-    expect(store.getState().home.isSearching).toBeTruthy();
-
-    await Promise.resolve(promise);
-
-    expect(store.getState().home.error).toBeFalsy();
-    expect(store.getState().home.searchResults).toEqual(expectedResponse);
-  });
-
-  test('should dispatch search location and return error state', async () => {
-    mock
-      .onGet('/location/search/', { params: { query: 'london' } })
-      .networkErrorOnce();
-
-    const promise = store.dispatch(searchLocation({ query: 'london' }));
-
-    expect(store.getState().home.isSearching).toBeTruthy();
-
-    await Promise.resolve(promise);
-
-    expect(store.getState().home.error).toBeTruthy();
-  });
-
-  test('should dispatch get location and return success state', async () => {
-    const expectedResponse = {
-      consolidated_weather: [
+    test('should dispatch search location and return success state', async () => {
+      const expectedResponse = [
         {
-          id: 5102173131112448,
-          applicable_date: '2021-04-04',
-          min_temp: 2.055,
-          max_temp: 15.66,
+          title: 'London',
+          location_type: 'City',
+          woeid: 44418,
+          latt_long: '51.506321,-0.12714',
         },
-      ],
-      parent: {
-        title: 'England',
-        location_type: 'Region / State / Province',
-        woeid: 24554868,
-        latt_long: '52.883560,-1.974060',
-      },
-      title: 'London',
-    };
+      ];
 
-    mock.onGet('/location/123/').replyOnce(200, expectedResponse);
+      mock
+        .onGet('/location/search/', { params: { query: 'london' } })
+        .replyOnce(200, expectedResponse);
 
-    expect(store.getState().home.isLoading).toBeFalsy();
+      expect(store.getState().home.isSearching).toBeFalsy();
 
-    const promise = store.dispatch(getLocation(123));
+      const promise = store.dispatch(searchLocation({ query: 'london' }));
 
-    expect(store.getState().home.isLoading).toBeTruthy();
+      expect(store.getState().home.isSearching).toBeTruthy();
 
-    await Promise.resolve(promise);
+      await Promise.resolve(promise);
 
-    expect(store.getState().home.error).toBeFalsy();
-    expect(store.getState().home.locationData).toEqual(expectedResponse);
-  });
+      expect(store.getState().home.error).toBeFalsy();
+      expect(store.getState().home.searchResults).toEqual(expectedResponse);
+    });
 
-  test('should dispatch get location and return error state', async () => {
-    mock.onGet('/location/123/').networkErrorOnce();
+    test('should dispatch search location and return error state', async () => {
+      mock
+        .onGet('/location/search/', { params: { query: 'london' } })
+        .networkErrorOnce();
 
-    const promise = store.dispatch(getLocation(123));
+      const promise = store.dispatch(searchLocation({ query: 'london' }));
 
-    expect(store.getState().home.isLoading).toBeTruthy();
+      expect(store.getState().home.isSearching).toBeTruthy();
 
-    await Promise.resolve(promise);
+      await Promise.resolve(promise);
 
-    expect(store.getState().home.error).toBeTruthy();
+      expect(store.getState().home.error).toBeTruthy();
+    });
+
+    test('should dispatch get location and return success state', async () => {
+      const expectedResponse = {
+        consolidated_weather: [
+          {
+            id: 5102173131112448,
+            applicable_date: '2021-04-04',
+            min_temp: 2.055,
+            max_temp: 15.66,
+          },
+        ],
+        parent: {
+          title: 'England',
+          location_type: 'Region / State / Province',
+          woeid: 24554868,
+          latt_long: '52.883560,-1.974060',
+        },
+        title: 'London',
+      };
+
+      mock.onGet('/location/123/').replyOnce(200, expectedResponse);
+
+      expect(store.getState().home.isLoading).toBeFalsy();
+
+      const promise = store.dispatch(getLocation(123));
+
+      expect(store.getState().home.isLoading).toBeTruthy();
+
+      await Promise.resolve(promise);
+
+      expect(store.getState().home.error).toBeFalsy();
+      expect(store.getState().home.locationData).toEqual(expectedResponse);
+    });
+
+    test('should dispatch get location and return error state', async () => {
+      mock.onGet('/location/123/').networkErrorOnce();
+
+      const promise = store.dispatch(getLocation(123));
+
+      expect(store.getState().home.isLoading).toBeTruthy();
+
+      await Promise.resolve(promise);
+
+      expect(store.getState().home.error).toBeTruthy();
+    });
   });
 });
